04178be2e4451a67d52f4e5dcadf35463bf31d1c
Isaac Granados Gomez <isaacxdd@gmail.com>
2025-10-12 20:28:09 -0600

    Use slug data for Servicios grid links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    darkMode: 'class',
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
-    theme: { extend: {} },
-    plugins: [],
-}
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: { extend: {} },
+  plugins: [require('@tailwindcss/line-clamp')],
+};
